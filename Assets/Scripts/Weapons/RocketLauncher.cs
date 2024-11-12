1dd3524ca44807493d0d99bb4211e3c51cfa7914
olveckymate <mateolvecky@gmail.com>
2024-11-12 16:10:10 +0100

    RocketLauncher fire

--- Assets/Scripts/Weapons/RocketLauncher.cs
+++ Assets/Scripts/Weapons/RocketLauncher.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 public class RocketLauncher : Weapon
 {
     /// <summary>
-    /// Fires the Rocket Launcher.
+    /// Fires the rocket launcher.
     /// </summary>
+    /// 
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 3f;
     public override void Fire()
     {
         // Implement the firing logic for the RocketLauncher
-        Debug.Log("RocketLauncher fired!");
+        Debug.Log("RocketLauncher Fired!");
+
+        if (projectilePrefab != null)
+        {
+            Vector3 firePosition = GameObject.FindWithTag("Player").transform.position;
+            GameObject projectile = Instantiate(projectilePrefab, firePosition, Quaternion.identity);
+
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                rb.velocity = Vector2.up * projectileSpeed;
+            }
+        }
     }
 }
